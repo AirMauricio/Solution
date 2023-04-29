b3f1d96bdf129492ff16fd5b719ca5f787597f5e
Osca Gama <23.oscarr@gmail.com>
2023-04-29 13:15:24 -0500

    Primer Inser con API

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask
-from flask_mysqldb import MySQL
-import mysql.connector
+from flask import Flask, jsonify
 import pandas as pd
+import pymysql
 
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'HR'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+@app.route('/load_data')
+def load_data():
+    # Leer los datos del archivo CSV con pandas
+    data = pd.read_csv('transform_PDF_CSV/csv_tables/departamentos/departamentos.csv', sep=";")
 
-mysql = MySQL()
-mysql.init_app(app)
+    # Conectar con la base de datos MySQL
+    conn = pymysql.connect(host='localhost',
+                           user='root',
+                           password='',
+                           db='HR')
+    cursor = conn.cursor()
+
+    # Insertar los datos en la tabla de la base de datos
+    for i, row in data.iterrows():
+        query = f"INSERT INTO HR.departments (id, department) VALUES ('{row['id']}', '{row['department']}')"
+        cursor.execute(query)
+    conn.commit()
+
+    # Cerrar la conexión con la base de datos
+    cursor.close()
+    conn.close()
+
+    # Devolver una respuesta JSON con los datos insertados
+    return jsonify({'message': 'Los datos han sido cargados correctamente'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,7 @@
+import app
+
+def run():
+    app.cargar_historico()
+
+if __name__ == '__main__':
+    run()
